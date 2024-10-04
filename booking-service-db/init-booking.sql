521caedfe4dbc6518c1705a689b931628c76691d
ryantoxx <nikitachihai2@gmail.com>
2024-10-05 01:28:34 +0300

    feat: add bookingdb

---
+++ booking-service-db/init-booking.sql
@@ -0,0 +1,22 @@
+\c bookingdb
+
+CREATE TABLE IF NOT EXISTS properties (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    price_per_night FLOAT NOT NULL,
+    availability BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    start_date VARCHAR(50) NOT NULL,
+    end_date VARCHAR(50) NOT NULL,
+    status VARCHAR(50) DEFAULT 'confirmed',
+    CONSTRAINT fk_property
+      FOREIGN KEY (property_id) 
+      REFERENCES properties(id)
+      ON DELETE CASCADE
+);
