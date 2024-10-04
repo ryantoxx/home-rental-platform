eb337dc95545e9b2f4c7b385604ae50caaf4c9a8
ryantoxx <nikitachihai2@gmail.com>
2024-10-05 01:27:01 +0300

    feat: add userdb

---
+++ user-service-db/init-user.sql
@@ -0,0 +1,10 @@
+
+\c user_service_db
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(512) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
